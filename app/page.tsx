696d111ff7ac75bae5002371b50552b344b4024a
Yash-M-eng <yashmalawant9@gmail.com>
2026-05-29 13:16:48 +0530

    Set up

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-1 items-center justify-center bg-background px-6 text-center">
+      <h1 className="font-sans text-4xl font-semibold tracking-normal text-foreground sm:text-6xl">
+        Coming soon
+      </h1>
+    </main>
+  );
+}
